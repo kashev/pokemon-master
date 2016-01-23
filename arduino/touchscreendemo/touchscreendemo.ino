c7d3ce293056debfdbeb9e12794b0b10f97e27c9
Kashev Dalmia <kashev.dalmia@gmail.com>
2016-01-23 16:17:01 -0600

    changing the comm protocol on the arduino

--- arduino/touchscreendemo/touchscreendemo.ino
+++ arduino/touchscreendemo/touchscreendemo.ino
@@ -1,57 +1,66 @@
-// Touch screen library with X Y and Z (pressure) readings as well
-// as oversampling to avoid 'bouncing'
-// This demo code returns raw readings, public domain
+// pokemon-master
 
 #include <stdint.h>
 #include "TouchScreen.h"
 
-#define YP A2  // must be an analog pin, use "An" notation!
-#define XM A3  // must be an analog pin, use "An" notation!
+#define YP A2  // must be an analog pin
+#define XM A3  // must be an analog pin
 #define YM A0  // can be a digital pin
 #define XP A1  // can be a digital pin
 
 #define BEAM 7 // digital I/O pin for sensing hand waving
 
-// For better pressure precision, we need to know the resistance
-// between X+ and X- Use any multimeter to read it
-// For the one we're using, its 300 ohms across the X plate
+/*
+ * For better pressure precision, we need to know the resistance
+ * between X+ and X- Use any multimeter to read it
+ * For the one we're using, its 300 ohms across the X plate
+ */
 TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
 
 // debounce counter for beam
-uint8_t beamDebounceCounter = 0; 
+uint8_t beamDebounceCounter = 0;
 
-void setup(void) {
+void setup(void)
+{
     Serial.begin(9600);
     pinMode(BEAM, INPUT_PULLUP);
 }
 
 
-void loop(void) {
+void loop(void)
+{
     // BEAM goes low if IR beam is broken
-    if (digitalRead(BEAM) == LOW) {
-        if (beamDebounceCounter == 0) {
-            Serial.println("BREAK");
-            beamDebounceCounter = 2;  // 200ms debounce timer
+    if (digitalRead(BEAM) == LOW)
+    {
+        if (beamDebounceCounter == 0)
+        {
+            Serial.println("!");
+            beamDebounceCounter = 2; // 200ms debounce timer
         }
-        else {
+        else
+        {
             beamDebounceCounter = 0;
         }
     }
 
-    // a point object holds x y and z coordinates
+    // a point object holds x y and z coordinates.
     TSPoint p = ts.getPoint();
 
     // we have some minimum pressure we consider 'valid'
     // pressure of 0 means no pressing!
-    if (p.z > ts.pressureThreshhold) {
-        Serial.print("X = "); 
+    if (p.z > ts.pressureThreshhold)
+    {
+        Serial.print("{\"X\": ");
         Serial.print(p.x);
 
-        Serial.print("\tY = "); 
+        Serial.print(", \"Y\": ");
         Serial.print(p.y);
 
-        Serial.print("\tPressure = "); 
-        Serial.println(p.z);
+        Serial.print(", \"P\": ");
+        Serial.print(p.z);
+
+        Serial.println("}");
+
     }
 
     delay(100);
